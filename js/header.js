554b50ee0a2b1a8235ff6501f51a17a7fbe98e38
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-09 20:33:37 -0300

    Efeito de cor scroll header

---
+++ js/header.js
@@ -0,0 +1,11 @@
+console.log('Hello World')
+const header = document.querySelector('#header');
+window.onscroll = function() {
+    let top = window.scrollY;
+    console.log(top);
+    if(top > 0) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+}
