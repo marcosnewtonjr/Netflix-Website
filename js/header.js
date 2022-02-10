8ed8abbd829d1cd11183eebea91c1ba242b3ff88
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-10 09:54:59 -0300

    Ajuste no background

--- js/header.js
+++ js/header.js
@@ -1,10 +1,11 @@
 const header = document.querySelector('#header');
+header.classList.remove('active');
 window.onscroll = function() {
     let top = window.scrollY;
     console.log(top);
-    if(top > 0) {
-        header.classList.add('active')
-    } else {
+    if(top == 0) {
         header.classList.remove('active')
+    } else {
+        header.classList.add('active')
     }
 }
