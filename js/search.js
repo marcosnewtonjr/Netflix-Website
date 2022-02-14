8b941f0ddff8b898bc4818a67204d4295c9e8ad1
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-14 01:49:14 -0300

    Update

--- js/search.js
+++ js/search.js
@@ -1,5 +1,18 @@
-const icon = document.querySelector('.search-icon')
-const box = document.querySelector('.search-box')
+let icon = document.querySelector('.search-icon')
+let box = document.querySelector('.search-box')
+let foco = document.querySelector('#search-box')
+let clear = document.querySelector('.clear-icon')
+let body = document.querySelector('body')
+
 icon.onclick = function() {
     box.classList.toggle('active')
+    foco.focus()
+    foco.value = ''
+}
+
+clear.onclick = function() {
+    if (foco.value.length > 0) {
+        foco.value = ''
+        foco.focus()
+    }
 }
