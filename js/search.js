e78f03380309a2ddddc11a6c64c76335383a2c81
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-15 21:26:14 -0300

    Update

--- js/search.js
+++ js/search.js
@@ -3,7 +3,10 @@ let iconSearch = document.querySelector('.search-icon-box')
 let iconClear = document.querySelector('.clear-icon')
 let box = document.querySelector('.search-box')
 let search = document.querySelector('#search-box')
-let header = document.querySelector('#header')
+let close1 = document.querySelector('.background')
+let close2 = document.querySelector('#head')
+let close3 = document.querySelector('#header')
+let close4 = document.querySelector('.user')
 
 iconSearch.onclick = function() {
     if (box.style.width == '35px') {
@@ -11,9 +14,6 @@ iconSearch.onclick = function() {
         box.style.border = 'solid 1px white'
         box.style.backgroundColor = 'var(--darkTransparent)'
         search.focus()
-        if(search.value.length == 0) {
-            iconClear.style.display = 'none'
-        }
     } else {
         box.style.width = '35px'
         box.style.border = ''
@@ -22,12 +22,21 @@ iconSearch.onclick = function() {
     }
 }
 
+search.onkeyup = function() {
+    let campo = search.length
+    if(campo != 0) {
+        iconClear.style.display = 'block'
+    }
+}
+
 iconClear.onclick = function() {
     search.value = ''
+    iconClear.style.display = 'none'
+    search.focus()
 }
 
 window.onclick = function(e) {
-    if (e.target == header) {
+    if (e.target == close1 || e.target == close2 || e.target == close3 || e.target == close4) {
         box.style.width = '35px'
         box.style.border = ''
         box.style.backgroundColor = ''
